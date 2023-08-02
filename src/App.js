53b8cc0bb6eabe3e6480baed9ea7af6d660b1c98
hb7098 <hb7098@rit.edu>
2023-08-02 09:21:29 -0400

    pdf to jpg conversion fixed ipfs storage service switched to nft.storage

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import Header from './Header';
 import certImage from './foundry-certificate-image.PNG';
 import './App.css'
 import { Web3Storage } from 'web3.storage';
-import { Web3, validator} from 'web3'; // Import web3 library
+import { Web3 } from 'web3'; // Import web3 library
 import { MyTokenABI } from './MyTokenABI'; // ABI
 import { PDFDocument, StandardFonts } from 'pdf-lib';
+import { NFTStorage, File } from 'nft.storage';
+import workerSrc from 'pdfjs-dist/build/pdf.worker.entry';
+const pdfjsLib = require ("pdfjs-dist");
+pdfjsLib.GlobalWorkerOptions.workerSrc = workerSrc;
 
 const ethers = require("ethers");
-
+const NFT_STORAGE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDMyYTQzZDkxQTlhZDNFMTg2N2EwMzM1NUEwQzIwYTUwNTgyYjVFMkMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY5MDc2NjQ0NjQxOSwibmFtZSI6IktleTEifQ.LVhaSrgeiumXeTjI7JzhMRjjLOW7P1xU36KPdgTz2HY";
 const fontSize = 20;
 
 const customerArray = [
   {
     name : "Kirsty Gallegos",
     cert : ["RIT Bachelor's in Computer Engineering", "RIT Master's in Computer Engineering"],
-    addr : "0x39D28E1A30c8C2b0C208D0714ae6a09Fc97261Ce"
+    addr : "0x234f5811e02C85f1cB28D5088d6Ff0D29E72323B"
   },
   {
     name : "Miranda Salazar",
@@ -40,6 +44,26 @@ const customerArray = [
 ]
 var selectedCustomer;
 
+function dataURItoBlob(dataURI) {
+  // const byteString = atob(dataURI.split(',')[1]);
+  // const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+  const base64Index = dataURI.indexOf(';base64,');
+  if (base64Index === -1) {
+    throw new Error('Invalid data URI: Missing ";base64,"');
+  }
+  const base64 = dataURI.substring(base64Index + 8);
+  const byteString = atob(base64);
+  const mimeString = dataURI.substring(5, base64Index);
+  const arrayBuffer = new ArrayBuffer(byteString.length);
+  const uint8Array = new Uint8Array(arrayBuffer);
+
+  for (let i = 0; i < byteString.length; i++) {
+    uint8Array[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([arrayBuffer], { type: mimeString });
+}
+
 // main function starts
 const FileUploader = () => {
 
@@ -49,6 +73,8 @@ const FileUploader = () => {
   const [web3, setWeb3] = useState(null);
   const [contract, setContract] = useState(null);
   const [tokenId, setTokenId] = useState('');
+  const [jpg, setjpg] = useState('');
+
 
   // uploadFileToWeb3Storage
   // PDF Creation & Uploading to web3.storage
@@ -100,16 +126,60 @@ const FileUploader = () => {
           font
         });
       }
-
       const pdfBytes = await pdfDoc.save();
-      const fileToUpload = new File([pdfBytes], 'example.pdf');
 
-      // Upload to web3.storage
+      const loadingTask = pdfjsLib.getDocument(pdfBytes);
+      const canvas = document.createElement("canvas");
+      const pdfDocument = await loadingTask.promise;
+      const pdfpage = await pdfDocument.getPage(1);
+      const viewport = pdfpage.getViewport({ scale : 1.0 });
+      canvas.width = viewport.width;
+      canvas.height = viewport.height;
+      const ctx = canvas.getContext("2d");
+      var task = pdfpage.render({canvasContext : ctx, viewport: viewport});
+      task.promise.then(async function(){
+        var jpg = canvas.toDataURL('image/jpeg');
+        const blob = dataURItoBlob(jpg);
+        const pdfUpload = new File([blob], 'example.jpg', { type : 'image/jpg' });
+        const nftstorage = new NFTStorage({
+          token : NFT_STORAGE_KEY
+        });
+        
+        if (pdfUpload) {
+          const returnedData = await nftstorage.store({
+            name: 'Certificate for ' + selectedCustomer,
+            description: 'Description',
+            image: pdfUpload,
+          });
+          console.log(returnedData);
+          console.log('IPFS URL for the metadata:', returnedData.url)
+          console.log('metadata.json contents:\n', returnedData.data)
+          setCID(returnedData.url);
+        }
+      })
+
+
+      // const pdfUpload = new File([pdfBytes], 'example.pdf', { type : 'application/pdf'});
+      // const imageUpload = new File([fetchimage], 'fetchedimage.jpg');
+      // Upload to web3.storage - deprecated
       // Change apikey as needed
-      const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEU2ODU2MThDRDRkNzg1NTFkMTY5NzU4MzhEMTNGQ0JkMzZlNTlGYWEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODk0NzYxNDkyMTYsIm5hbWUiOiJJRiJ9.qhmsZBWTLWcv1Gp3pVp9evzCm3wVOZ1gbV1uNP4l9uo'; // Replace with your web3.storage API key
-      const client = new Web3Storage({ token: apiKey });
-      const cid = await client.put([fileToUpload]);
-      setCID(cid);
+      // const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEU2ODU2MThDRDRkNzg1NTFkMTY5NzU4MzhEMTNGQ0JkMzZlNTlGYWEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODk0NzYxNDkyMTYsIm5hbWUiOiJJRiJ9.qhmsZBWTLWcv1Gp3pVp9evzCm3wVOZ1gbV1uNP4l9uo'; // Replace with your web3.storage API key
+      // const client = new Web3Storage({ token: apiKey });
+      // const PDFcid = await client.put([pdfUpload]);
+      // console.log("PDF uploaded. CID : ", PDFcid);
+      // const metaData = {
+      //   name : "testing certificate for " + selectedCustomer,
+      //   description : "this is a testing certificate",
+      //   image : "ipfs://" + PDFcid
+      // }
+      // console.log(metaData);
+      // const jsonString = JSON.stringify(metaData);
+      // const metaDataUpload = new Blob([jsonString], { type : 'application/json' });
+      // const metaDataCID = await client.put([metaDataUpload], 'metadata.json');
+      // console.log("metadata uploaded. CID : ", metaDataCID);
+      // setCID(metaDataCID);
+
+      
     } catch (error) {
       console.error('Error uploading file to web3.storage:', error);
     }
@@ -155,7 +225,19 @@ const FileUploader = () => {
   
       // Process the logs to get the tokenId
       const tokenId = logs[0]?.topics[3]; // tokenId is the 4th topic in the Transfer event
-      console.log("Retrieved : ", tokenId);
+      //testing
+      const txParam = {
+        to: "0x12068e7a7e2755b46AcffcB9933Ae0Ca519B2036",
+        'data': contract.methods.ownerOf(tokenId).encodeABI(),
+      };
+  
+      const txHash = await window.ethereum.request({
+        method: 'eth_call',
+        params: [txParam],
+      });
+
+      console.log("Testing returned : ", txHash);
+      //testing ends
 
       return tokenId;
     } catch (error) {
@@ -212,7 +294,7 @@ const FileUploader = () => {
         const txParam = {
           to: "0x12068e7a7e2755b46AcffcB9933Ae0Ca519B2036",
           from: window.ethereum.selectedAddress,
-          'data': contract.methods.mintToken(window.ethereum.selectedAddress, cid).encodeABI(),
+          'data': contract.methods.mintToken(customer.addr, cid).encodeABI(),
         };
     
         const txHash = await window.ethereum.request({
@@ -238,12 +320,11 @@ const FileUploader = () => {
   }
   const initWeb3 = async () => {
     const web3Instance = new Web3('https://rpc.sepolia.dev'); // Connect to Ganache RPC endpoint
-    var privateKeyString = '23f2da6da69a9708b0fd8c58a6a9b6f2d3e407d9d9dbde861d7a34a90930cf0a';
-    privateKeyString = web3Instance.utils.sha3(privateKeyString);
-    const account = web3Instance.eth.accounts.wallet.add(privateKeyString);
-    const isValidPrivateKey = validator.isHexStrict(privateKeyString) && privateKeyString.length === 64;
-    console.log('Is Valid Private Key:', isValidPrivateKey);
-
+    // var privateKeyString = '23f2da6da69a9708b0fd8c58a6a9b6f2d3e407d9d9dbde861d7a34a90930cf0a';
+    // privateKeyString = web3Instance.utils.sha3(privateKeyString);
+    // const account = web3Instance.eth.accounts.wallet.add(privateKeyString);
+    // const isValidPrivateKey = validator.isHexStrict(privateKeyString) && privateKeyString.length === 64;
+    // console.log('Is Valid Private Key:', isValidPrivateKey);
     // setAccount(account);
     setWeb3(web3Instance);
   };
